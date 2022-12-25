b5237df3bbaa111b40224fa8bf2795ad81202a16
Vladimir Kazantsev <gordiancrew@gmail.com>
2022-12-25 17:45:31 +0300

    feat: add content section

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import MainPage from "./components/main/header";
 import "./components/styles/global.css";
 import Header from "./components/main/header";
 import Picture from "./components/main/picture";
+import Content from "./components/main/content";
 
 function App() {
   return (
     <div>
       <Header />
       <Picture />
+      <Content />
       <h1>Преврати свое устройство в игровое поле!!!</h1>
       <h1 className="test">We are start!&</h1>
       <h1>We are start!</h1>
---
+++ src/components/main/content.tsx
@@ -0,0 +1,44 @@
+import "../styles/global.css";
+import "../styles/content.css";
+import server from "../../images/ImageServerHost.png";
+
+function Content() {
+  return (
+    <div className="content">
+      <div className="content__section1">
+        <div className="content__section1-left">
+          <img src={server} className="content__section-left-img"></img>
+        </div>
+        <div className="content__section1-right">
+          <div className="content__text-step1">
+            <div className="content__step1-linia linia"></div>
+            <div className="content__text1">Облачный гейминг</div>
+          </div>
+          <div className="content__text2">Как это работает?</div>
+          <div className="content__text3">
+            Наши сервера запускают игру, и передают вам картинку, которой вы
+            можете управлять в реальном времени!
+          </div>
+          <div className="content__linia2"></div>
+          <div className="content__text3">
+            Приложение запустится на ПК с процессором от 1.5 GHz , от 1 Gb RAM и
+            доступом в интернет от 15 мбит/с на всех операционых системах
+            Windows 7, 8, 10 MacOS и Linux .
+          </div>
+
+          <div className="content__text4">Загрузить</div>
+          <div className="content__linia3"></div>
+        </div>
+      </div>
+      <div className="content__section2">
+      <div className="content__text-step1">
+            <div className="content__step1-linia linia"></div>
+            <div className="content__text1">Быстрый старт</div>
+          </div>
+          <div className="content__text2">Начни играть</div>
+
+      </div>
+    </div>
+  );
+}
+export default Content;
