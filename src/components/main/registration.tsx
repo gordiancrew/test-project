1cb28834db29c5c8e1bdbe3cc193aaf8cb6f9019
Vladimir Kazantsev <gordiancrew@gmail.com>
2023-01-04 00:07:13 +0300

    feat: add useState

--- src/components/main/registration.tsx
+++ src/components/main/registration.tsx
@@ -1,15 +1,17 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 import "../styles/global.css";
 import "../styles/registration.css";
 
 function Registration() {
-  let isLoginValid = false;
-  let isPasswordValid = false;
+  const [isLoginValid, setLoginValid] = useState(false);
+  // let isPasswordValid = false;
+  const[isPasswordValid, setPasswordValid]=useState(false);
   let isCheckbox = false;
-  let username = '?';
+  const[userName, setUserName]=useState('?');
   function checkBoxChange() {
-
-    let checkBox = document.querySelector('.registration__checkbox') as HTMLInputElement;
+    let checkBox = document.querySelector(
+      ".registration__checkbox"
+    ) as HTMLInputElement;
     if (checkBox.checked) {
       isCheckbox = true;
       checkBtn();
@@ -17,87 +19,75 @@ function Registration() {
       isCheckbox = false;
       checkBtn();
     }
-
-
   }
 
   function checkBtn() {
-    let btn = document.querySelector('.btn') as HTMLElement;
+    let btn = document.querySelector(".btn") as HTMLElement;
 
     if (isLoginValid && isPasswordValid && isCheckbox) {
-      btn.classList.add('registration__let-reg-activ');
+      btn.classList.add("registration__let-reg-activ");
       btn.onclick = function () {
-        let registration = document.querySelector('.registration');
-        let pop = document.createElement('div');
-        pop.classList.add('pop');
-        pop.innerText = 'Hello ' + username;
+        let registration = document.querySelector(".registration");
+        let pop = document.createElement("div");
+        pop.classList.add("pop");
+        pop.innerText = "Hello " + userName;
         registration?.appendChild(pop);
 
-
-
-        setTimeout(() => window.location.href = '/', 3000);
-
-
-      }
+        setTimeout(() => (window.location.href = "/"), 3000);
+      };
     } else {
-
-      btn.classList.remove('registration__let-reg-activ');
+      btn.classList.remove("registration__let-reg-activ");
       btn.onclick = function () {
-
-        const message: string = (!isLoginValid ? 'Некорректный email адресс  \n' : '') +
-         ( !isPasswordValid ? 'Пароль должен состоять минимум из 8 символов и не больше 20\n' : '') +
-           ( !isCheckbox ? 'Нужно принять условия!\n' : '');
-        alert(
-          message
-        );
-      }
+        const message: string =
+          (!isLoginValid ? "Некорректный email адресс  \n" : "") +
+          (!isPasswordValid
+            ? "Пароль должен состоять минимум из 8 символов и не больше 20\n"
+            : "") +
+          (!isCheckbox ? "Нужно принять условия!\n" : "");
+        alert(message);
+      };
     }
   }
 
   function validateEmail(email: string) {
-    let validEmail = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    let validEmail =
+      /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
     if (validEmail.test(email) == false) {
       return false;
-    } else { return true }
+    } else {
+      return true;
+    }
   }
 
-  function nameChange(e: ChangeEvent) {
-    let login = document.querySelector('.login') as HTMLInputElement;
-
-
-    if (validateEmail(login.value)) {
-      // inp.style.borderColor='green';
-      login.classList.add('border-green');
-      login.classList.remove('border-red');
-      isLoginValid = true;
-      username = login.value;
+  function nameChange(e: ChangeEvent<HTMLInputElement>) {
+    if (validateEmail(e.target.value)) {
+      e.target.classList.add("border-green");
+      e.target.classList.remove("border-red");
+      setLoginValid(true);
+      setUserName(e.target.value);
       checkBtn();
     } else {
-      login.classList.remove('border-green');
-      login.classList.add('border-red');
-      isLoginValid = false;
+      e.target.classList.remove("border-green");
+      e.target.classList.add("border-red");
+      setLoginValid(false);
       checkBtn();
     }
-
   }
 
-  function passwordChange(e: ChangeEvent) {
-    let password = document.querySelector('.password') as HTMLInputElement;
-    if (password.value.length >= 8 && password.value.length < 20) {
-      password.classList.add('border-green');
-      password.classList.remove('border-red');
-      isPasswordValid = true;
+  function passwordChange(e: ChangeEvent<HTMLInputElement>) {
+    if (e.target.value.length >= 8 && e.target.value.length < 20) {
+      e.target.classList.add("border-green");
+      e.target.classList.remove("border-red");
+      setPasswordValid(true);
       checkBtn();
     } else {
-      password.classList.remove('border-green');
-      password.classList.add('border-red');
-      isPasswordValid = false;
+      e.target.classList.remove("border-green");
+      e.target.classList.add("border-red");
+      setPasswordValid(false);
       checkBtn();
     }
-
   }
-
 
   return (
     <div className="registration">
@@ -110,21 +100,23 @@ function Registration() {
           <input
             className="registration__form-input login"
             placeholder="Почта/Телефон"
-
-            onChange={e => nameChange(e)}
+            onChange={(e) => nameChange(e)}
           />
           <input
             className="registration__form-input password"
             placeholder="Пароль"
-            type='password'
-            onChange={e => passwordChange(e)}
+            type="password"
+            onChange={(e) => passwordChange(e)}
           ></input>
         </form>
         <div className="registration__let-reg btn">Зарегестрироваться</div>
         <div className="registration__agree-box">
           <form>
-            <input type="checkbox" className="registration__checkbox"
-              onClick={checkBoxChange}></input>
+            <input
+              type="checkbox"
+              className="registration__checkbox"
+              onClick={checkBoxChange}
+            ></input>
           </form>
           <div className="registration__text-agree">
             Я соглашаюсь со следующими установленными правилами: Политика
@@ -135,7 +127,6 @@ function Registration() {
             конфиденциальности портала WARPLAY.CLOUD, Политика
             конфиденциальности WARPLAY.CLOUD для детей
           </div>
-
         </div>
         <div className="registration__alter-enter ">
           <div className="registration__linia"></div>
